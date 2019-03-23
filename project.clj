0cb78d2fc753fbf68101fd42fcda0a6bc001da18
qrt <kh@sharpthoughts.net>
2019-03-23 14:23:24 +0100

    Update license details in project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject templates "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "MIT"
-            :url "https://opensource.org/licenses/MIT"}
+  :description "Produce values from templates"
+  :url "https://github.com/qrthey/templates.git"
+  :license {:name "Beerware-alike"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns templates.core})

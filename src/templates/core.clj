118a383e3a6b29a11e7130b25dc9d5f92e2a832a
qrt <kh@sharpthoughts.net>
2019-03-28 12:11:07 +0100

    Expose fill-out-template in api

--- src/templates/core.clj
+++ src/templates/core.clj
@@ -18,7 +18,28 @@
       (keyword (filler nspace) (filler n))
       (keyword (filler n)))))
 
-(defn- fill-out-template
+(defn fill-out-template
+  "Takes a template that can be any clojure datastructure and a
+  replacement maps. A replacement map describes replacement values to
+  create a value from the template. Keys in a replacement map must be
+  strings.
+
+  Returns a value that is an exact copy of the template, except for
+  strings, keywords and pottentially symbols.
+
+  Strings are transformed with clojure.string/replace for each
+  key/value pair in the replacement map. The key in a pair is used as
+  the match value while the string representation of the value is used
+  as the replacement.
+
+  For keywords, both the namespace and the name are treated as strings
+  and then recomposed in a new keyword. Non namespaced keywords are
+  supported as well.
+
+  For symbols, a check is made to see if the name of the symbol
+  exactly matches a key in the replacement map, and if so, it gets
+  substituted by the value for that key, otherwise the original symbol
+  is kept in the resulting value."
   [template replacements]
   (cond
     (sequential? template)
@@ -39,32 +60,5 @@
 
     (and (symbol? template) (contains? replacements (name template)))
     (get replacements (name template))
-    
+
     :default template))
-
-(defn template->values
-  "Takes a template that can be any clojure datastructure and a sequence
-  of replacement maps. A replacement map describes replacement values
-  to create a value from the template. Keys in a replacement map must
-  be strings.
-
-  Returns per replacement map a value that is an exact copy of the
-  template, except for strings, keywords and pottentially symbols.
-
-  Strings are transformed with clojure.string/replace for each
-  key/value pair in a replacement map. The key in the pair is used as
-  the match value while the string representation of the value is used
-  as the replacement.
-  
-  For keywords, both the namespace and the name are treated as strings
-  and then recomposed in a new keyword. Non namespaced keywords are
-  supported as well.
-
-  For symbols, a check is made to see if the name of the symbol
-  exactly matches a key in the replacement map, and if so, it gets
-  substituted by the value for that key, otherwise the original symbol
-  is kept in the resulting value."
-  [template replacements]
-  (map #(fill-out-template template %)
-       replacements))
-
--- test/templates/core_test.clj
+++ test/templates/core_test.clj
@@ -2,43 +2,23 @@
   (:require [clojure.test :refer :all]
             [templates.core :refer :all]))
 
-(deftest template->values-test
+(deftest fill-out-template-test
   (testing "Strings, keywords and symbols are handled correctly."
-    (is (= '([:a :deeply
+    (is (= [:a :deeply
               {:nested [:data-structure
                         {:id 1,
-                         :status review,
+                         :status 'review,
                          :prop-age "val recorded = 20",
                          :verified? :yeah}]}]
-             [:a :deeply
-              {:nested [:data-structure
-                        {:id 2,
-                         :status review,
-                         :prop-phone "val recorded = 20 30 40 50",
-                         :verified? :nope}]}]
-             [:a :deeply
-              {:nested [:data-structure
-                        {:id 3,
-                         :status review,
-                         :prop-last-gps-position "val recorded = 298EW9879DDK",
-                         :verified? :nope}]}])
 
-           (template->values 
+           (fill-out-template
             [:a :deeply
              {:nested [:data-structure
                        {:id '<<ID>>
                         :status 'review
                         :prop-<<PROP-NAME>> "val recorded = <<PROP-VAL>>"
                         :verified? '<<IS-VERIFIED?>>}]}]
-            [{"<<ID>>" 1
+            {"<<ID>>" 1
               "<<PROP-NAME>>" "age"
               "<<PROP-VAL>>" 20
-              "<<IS-VERIFIED?>>" :yeah}
-             {"<<ID>>" 2
-              "<<PROP-NAME>>" "phone"
-              "<<PROP-VAL>>" "20 30 40 50"
-              "<<IS-VERIFIED?>>" :nope}
-             {"<<ID>>" 3
-              "<<PROP-NAME>>" "last-gps-position"
-              "<<PROP-VAL>>" "298EW9879DDK"
-              "<<IS-VERIFIED?>>" :nope}])))))
+              "<<IS-VERIFIED?>>" :yeah})))))
